f3396d409ab6ea8a969162274905fb3893c761d2
suzzang <soochoi0325@gmail.com>
2018-11-13 13:07:03 +0900

    #4_2차세미나 과제 - 파트 or 이름을 통해 GET

--- report2/src/main/java/org/sopt/report2/api/UserController.java
+++ report2/src/main/java/org/sopt/report2/api/UserController.java
@@ -1,15 +1,17 @@
 package org.sopt.report2.api;
 
+import lombok.extern.slf4j.Slf4j;
 import org.sopt.report2.model.User;
 import org.springframework.web.bind.annotation.*;
+import sun.rmi.runtime.Log;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
-import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
-
+@Slf4j
 @RestController
 public class UserController {
     private final static List<User> userList = new LinkedList<>();
@@ -18,46 +20,61 @@ public class UserController {
     User user3 = new User(1410408, "최수똥", "iOS");
     User user4 = new User(1310408, "최냥냥", "디자인");
     User user5 = new User(1210408, "최몽몽", "기획");
+    User user6 = new User(1110408, "최몽땅", "기획");
+
 
 
     @GetMapping("/")
     public String getCurrentTime() {
         Calendar calendar = Calendar.getInstance();
         java.util.Date date = calendar.getTime();
-        String today = (new SimpleDateFormat("yyyyMMddHHmmss").format(date));
+        String today = (new SimpleDateFormat("yyyy.MM.dd HH시mm분").format(date));
 
         return today;
     }
 
     @GetMapping("/users")
-    public List<User> getUserListandSearchByNameORPart(@RequestParam(value = "name",required = false) final String name) {
-        userList.add(user1);
-        userList.add(user2);
-        userList.add(user3);
-        userList.add(user4);
-        userList.add(user5);
-        List<User> resultList = new LinkedList<>();
+    public List<User> getUserListandSearchByNameORPart(@RequestParam(value = "name",required = false) final Optional<String> name,
+                                                       @RequestParam(value = "part",required = false) final Optional<String> part) {
 
-        for (User u : userList) {
-            if (u.getName() == name) {
+        if(userList.isEmpty()){
 
-                resultList.add(u);
+            userList.add(user1);
+            userList.add(user2);
+            userList.add(user3);
+            userList.add(user4);
+            userList.add(user5);
+            userList.add(user6);
+
+        }
+
+        if(name.isPresent()){
+            List<User> byNameList = new LinkedList<>();
+            for (User u : userList) {
+                if (u.getName().equals(name.get())) { //Optional 쓸 때 객체안의 String 값을 가져오기위해 get()메소드를 쓴다
+                    byNameList.add(u);
+                }
+            }
+            return byNameList;
+        }else{
+            if(part.isPresent()){
+                List<User> byPartList = new LinkedList<>();
+                for(User u : userList){
+                    if (u.getPart().equals(part.get())){
+                        byPartList.add(u);
+                    }
+                }
+                return byPartList;
+            }else{
+                List<User> resultList = new LinkedList<>();
+                resultList = userList;
                 return resultList;
             }
 
         }
 
-        return resultList;
     }
 
-//    public String searchSameName(@RequestParam(value = "name")final String name){
-//        return "Dd"; //for문 돌려가면서 비교 검색
-//    }
-
-//    @GetMapping("/users")
-//    public String searchSamePart(@RequestParam(value = "part")final String part){
-//        return "Dd"; //for문 돌려가면서 비교 검색
-//    }
 
 //    @GetMapping("/users/{user_idx}")
 //    public String searchSameId(@PathVariable(value = "user_idx")final int user_idx){
